6ecaf31a5e0a37c0a70b94f9b10bf8cd97625cb7
GabrielAfflitto <gafflitto92@gmail.com>
2022-07-26 16:35:49 -0600

    Created joins table migration for UserGroups

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_26_211058) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_26_215350) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_26_211058) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "user_groups", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "group_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_user_groups_on_group_id"
+    t.index ["user_id"], name: "index_user_groups_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -27,4 +36,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_26_211058) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "user_groups", "groups"
+  add_foreign_key "user_groups", "users"
 end
